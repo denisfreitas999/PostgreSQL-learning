c50c6a9e15f6c04a3313b3e94ffac45a11a5df14
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-11 13:23:41 -0300

    feat: :sparkles: Falando sobre programação no banco de dados, conhecendo funções no PostgreSQL, entendendo tipos em parâmetros e retornos, retornando valores em funções SQL.

---
+++ course-04/sql/comandos.sql
@@ -0,0 +1,74 @@
+--##########################################################
+--######################## Etapa 01 ########################
+--##########################################################
+
+-- $$$$$$$$$$$$$$$$$$$ Criando funções $$$$$$$$$$$$$$$$$$
+-- Corpo de funções
+CREATE FUNCTION nome_funcao() RETURNS tipo_funcao AS '
+		CORPO DAS FUNÇÃO
+	' LANGUAGE SQL;
+
+-- Primeira Função
+CREATE FUNCTION academico.primeira_funcao() RETURNS INTEGER AS '
+		SELECT (5 - 3) * 2
+	' LANGUAGE SQL;
+
+SELECT academico.primeira_funcao() AS resultado_primeiras_funcao;
+SELECT * FROM academico.primeira_funcao() AS resultado_primeiras_funcao;
+
+-- Segunda função | Com parâmetros
+CREATE FUNCTION academico.soma_dois_numeros(primeiro_numero INTEGER, segundo_numero INTEGER) 
+	RETURNS INTEGER AS '
+		SELECT primeiro_numero + segundo_numero
+	' LANGUAGE SQL;
+
+SELECT academico.soma_dois_numeros(1,2);
+
+CREATE FUNCTION academico.multiplica_dois_numeros(INTEGER, INTEGER) 
+	RETURNS INTEGER AS '
+		SELECT $1 + $2
+	' LANGUAGE SQL;
+
+SELECT academico.multiplica_dois_numeros(2,2);
+
+-- Criando uma função de inserção na tabela aluno
+SELECT * FROM academico.aluno;
+
+CREATE FUNCTION academico.inserir_aluno(primeiro_nome VARCHAR, ultimo_nome VARCHAR, data_nascimento DATE)
+	RETURNS VARCHAR AS '
+		INSERT INTO academico.aluno (primeiro_nome, ultimo_nome, data_nascimento) 
+			VALUES (primeiro_nome, ultimo_nome, data_nascimento);
+	
+		SELECT primeiro_nome;
+	' LANGUAGE SQL;
+
+SELECT academico.inserir_aluno('Marcelo Henrique', 'Lima Barreto', '2000-11-07');
+
+-- Sobrescrevendo função
+
+CREATE OR REPLACE FUNCTION academico.inserir_aluno 
+	(primeiro_nome VARCHAR, ultimo_nome VARCHAR, data_nascimento DATE)
+	RETURNS VOID AS '
+		INSERT INTO academico.aluno (primeiro_nome, ultimo_nome, data_nascimento) 
+			VALUES (primeiro_nome, ultimo_nome, data_nascimento)
+	' LANGUAGE SQL;
+
+DROP FUNCTION academico.inserir_aluno(character varying,character varying,date);
+SELECT academico.inserir_aluno('Marcelo Henrique', 'Lima Barreto', '2000-11-07');
+SELECT * FROM academico.aluno;
+DELETE FROM academico.aluno
+	WHERE id BETWEEN 11 AND 13;
+
+-- Delimitação $$ $$ para utilizar strings dentro das funções
+CREATE FUNCTION academico.mock_aluno() RETURNS VOID AS $$
+	INSERT INTO academico.aluno (primeiro_nome, ultimo_nome, data_nascimento)
+		VALUES ('Benjamin', 'Freitas', '2018-05-14');
+$$ LANGUAGE SQL;
+
+SELECT academico.mock_aluno();
+SELECT * FROM academico.aluno;
+
+
+
+
+
