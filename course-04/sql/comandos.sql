cb9110e7983f005cde390ca4c4afe135be1938bf
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-11 14:43:00 -0300

    feat: :sparkles: Adicionando conteúdo que faltou da Etapa 02

--- course-04/sql/comandos.sql
+++ course-04/sql/comandos.sql
@@ -104,4 +104,67 @@ SELECT * FROM cria_instrutor_falso();
 -- Linha de dados
 SELECT cria_instrutor_falso();
 
--- 
+-- Retornando conjuntos
+INSERT INTO instrutor (nome, salario) VALUES ('Antônio Dias', 200);
+INSERT INTO instrutor (nome, salario) VALUES ('Benjamin Santos', 300);
+INSERT INTO instrutor (nome, salario) VALUES ('Nidivaldo Alves', 400);
+INSERT INTO instrutor (nome, salario) VALUES ('Angela Maria', 500);
+INSERT INTO instrutor (nome, salario) VALUES ('Maria José', 600);
+
+SELECT * FROM  instrutor;
+
+-- Criando função para retornar UM CONJUNTO (SETOF) DE INSTRUTORES que recebem mais que determinado valor
+CREATE FUNCTION instrutores_bem_pagos(valor_salario DECIMAL) RETURNS SETOF instrutor AS $$
+	SELECT * FROM instrutor WHERE salario >= valor_salario; 
+$$ LANGUAGE SQL;
+
+SELECT * FROM instrutores_bem_pagos(300);
+SELECT * FROM instrutores_bem_pagos(400);
+
+-- Criando função para retornar UM CONJUNTO (TABLE) DE INSTRUTORES que recebem mais que determinado valor
+CREATE FUNCTION instrutores_bem_pagos2(valor_salario DECIMAL) 
+	RETURNS TABLE (id INTEGER, nome VARCHAR, salario DECIMAL) AS $$
+		SELECT * FROM instrutor WHERE salario >= valor_salario; 
+$$ LANGUAGE SQL;
+
+SELECT * FROM instrutores_bem_pagos2(300);
+SELECT * FROM instrutores_bem_pagos2(400);
+
+-- $$$$$$$$$$$$$ 3 FORMAS DISTINTAS DE TIPAGEM $$$$$$$$$$$$$
+-- Criando o tipo com o TYPE
+CREATE TYPE dois_valores_inteiros AS (soma INTEGER, produto INTEGER);
+
+CREATE FUNCTION soma_e_produto(primeiro_valor INTEGER, segundo_valor INTEGER) RETURNS dois_valores_inteiros AS $$
+	SELECT primeiro_valor + segundo_valor AS soma, primeiro_valor * segundo_valor AS produto;
+$$ LANGUAGE SQL;
+
+SELECT * FROM soma_e_produto(2,3);
+
+-- Utilizando o OUT
+CREATE OR REPLACE FUNCTION soma_e_produto2 (
+    IN primeiro_valor INTEGER, 
+    IN segundo_valor INTEGER, 
+    OUT soma INTEGER, 
+    OUT produto INTEGER
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    soma := primeiro_valor + segundo_valor;
+    produto := primeiro_valor * segundo_valor;
+END;
+$$;
+
+SELECT * FROM soma_e_produto2(3, 5);
+
+-- Utilizando o OUT e RECORD para criar tipos por baixo dos panos
+CREATE FUNCTION instrutores_bem_pagos3(valor_salario DECIMAL, OUT nome VARCHAR, OUT salario DECIMAL) 
+	RETURNS SETOF record AS $$ 
+    SELECT nome, salario FROM instrutor WHERE salario > valor_salario; 
+$$ LANGUAGE SQL;
+
+SELECT * FROM instrutores_bem_pagos3(300);
+
+--##########################################################
+--######################## Etapa 03 ########################
+--##########################################################
