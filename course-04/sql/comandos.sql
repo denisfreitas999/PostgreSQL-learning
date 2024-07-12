887d24c987425082908db900c3fe747f348f7c44
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-12 12:31:59 -0300

    feat: :sparkles: Retornando valores com RETURN NEXT, usando LOOP para operações repetidas, evitando loops infinitos com EXIT, utilizando WHILE e FOR, percorrendo queries com FOR IN.

--- course-04/sql/comandos.sql
+++ course-04/sql/comandos.sql
@@ -338,3 +338,80 @@ $$ LANGUAGE plpgsql;
 
 SELECT nome, salario_ok(instrutor) FROM instrutor;
 
+-- Utilizando o RETURN NEXT para retornar múltiplos valores
+CREATE OR REPLACE FUNCTION tabuada(numero INTEGER) RETURNS SETOF INTEGER AS $$
+	BEGIN
+		RETURN NEXT numero * 1;
+		RETURN NEXT numero * 2;
+		RETURN NEXT numero * 3;
+		RETURN NEXT numero * 4;
+		RETURN NEXT numero * 5;
+		RETURN NEXT numero * 6;
+		RETURN NEXT numero * 7;
+		RETURN NEXT numero * 8;
+		RETURN NEXT numero * 9;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM tabuada(2);
+
+-- $$$$$$$$$$$$$$$ Estruturas de Repetição $$$$$$$$$$$$$$$
+-- LOOP
+DROP FUNCTION tabuada;
+CREATE OR REPLACE FUNCTION tabuada(numero INTEGER) RETURNS SETOF VARCHAR AS $$
+	DECLARE multiplicador INTEGER DEFAULT 1;
+	BEGIN
+		LOOP
+			RETURN NEXT numero || ' x ' || multiplicador || ' = ' || numero * multiplicador;
+			multiplicador := multiplicador + 1;
+			EXIT WHEN multiplicador = 11;
+		END LOOP;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM tabuada(7);
+
+-- WHILE
+DROP FUNCTION tabuada;
+CREATE OR REPLACE FUNCTION tabuada(numero INTEGER) RETURNS SETOF VARCHAR AS $$
+	DECLARE multiplicador INTEGER DEFAULT 1;
+	BEGIN
+		WHILE multiplicador <= 10 LOOP
+			RETURN NEXT numero || ' x ' || multiplicador || ' = ' || numero * multiplicador;
+			multiplicador := multiplicador + 1;
+		END LOOP;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM tabuada(7);
+
+-- FOR
+DROP FUNCTION tabuada;
+CREATE OR REPLACE FUNCTION tabuada(numero INTEGER) RETURNS SETOF VARCHAR AS $$
+	DECLARE multiplicador INTEGER DEFAULT 1;
+	BEGIN
+		FOR multiplicador IN 1..10 LOOP
+			RETURN NEXT numero || ' x ' || multiplicador || ' = ' || numero * multiplicador;
+			multiplicador := multiplicador + 1;
+		END LOOP;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM tabuada(7);
+
+-- Testando conhecimento
+DROP FUNCTION instrutor_com_salario;
+CREATE FUNCTION instrutor_com_salario(OUT nome VARCHAR, OUT salario_ok VARCHAR) RETURNS SETOF record AS $$
+    DECLARE
+        instrutor instrutor;
+    BEGIN
+        FOR instrutor IN SELECT * FROM instrutor LOOP
+            nome := instrutor.nome;
+            salario_ok = salario_ok(instrutor.id);
+            
+            RETURN NEXT;
+        END LOOP;
+    END;
+$$ LANGUAGE plpgsql
+
+SELECT * FROM instrutor_com_salario();
