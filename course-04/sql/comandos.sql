e442d894c0e38ea2c6fb766344dce06eb61955d8
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-12 19:15:47 -0300

    feat: :sparkles: Criando funções realistas, utilizando variável FOUND em PLpgSQL, realizando logs de operações no banco de dados, fazendo cálculos com conversões de tipos.

--- course-04/sql/comandos.sql
+++ course-04/sql/comandos.sql
@@ -338,6 +338,10 @@ $$ LANGUAGE plpgsql;
 
 SELECT nome, salario_ok(instrutor) FROM instrutor;
 
+--##########################################################
+--######################## Etapa 05 ########################
+--##########################################################
+
 -- Utilizando o RETURN NEXT para retornar múltiplos valores
 CREATE OR REPLACE FUNCTION tabuada(numero INTEGER) RETURNS SETOF INTEGER AS $$
 	BEGIN
@@ -415,3 +419,89 @@ CREATE FUNCTION instrutor_com_salario(OUT nome VARCHAR, OUT salario_ok VARCHAR) 
 $$ LANGUAGE plpgsql
 
 SELECT * FROM instrutor_com_salario();
+
+--##########################################################
+--######################## Etapa 06 ########################
+--##########################################################
+
+-- Criando exemplos de funções utilizando o banco academico
+SELECT * FROM academico.aluno;
+SELECT * FROM academico.categoria;
+SELECT * FROM academico.curso;
+SELECT * FROM academico.aluno_curso;
+
+CREATE FUNCTION academico.cria_curso(nome_curso VARCHAR, nome_categoria VARCHAR) RETURNS void AS $$
+	DECLARE id_categoria INTEGER;
+	BEGIN
+		-- BUSCAR ID DA CATEGORIA ATRAVÉS DO NOME DA CATEGORIA
+		SELECT id INTO id_categoria
+			FROM academico.categoria WHERE academico.categoria.nome = nome_categoria;
+		-- VERIFICAR SE A CATEGORIA JÁ EXISTE
+		IF NOT FOUND THEN
+			-- SE NÃO HOUVER A CATEGORIA, INSERIR A CATEGORIA E RESGATAR O ID
+			INSERT INTO academico.categoria (nome) VALUES (nome_categoria) RETURNING id INTO id_categoria;
+		END IF;
+	
+		-- CRIAR O CURSO
+		INSERT INTO academico.curso (nome, categoria_id) VALUES (nome_curso, id_categoria);
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM academico.cria_curso('Introdução à Javascript', 'Tecnologia');
+SELECT * FROM academico.cria_curso('Conhecendo Gênesis', 'Religião');
+
+-- Verificar a estrutura da tabela instrutor
+SELECT * FROM instrutor;
+
+-- Criação da tabela de log
+CREATE TABLE log_instrutores (
+    id SERIAL PRIMARY KEY,
+    informacao VARCHAR(255),
+    momento_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Criação da função cria_instrutor
+DROP FUNCTION IF EXISTS cria_instrutor;
+CREATE OR REPLACE FUNCTION cria_instrutor (nome_instrutor VARCHAR, salario_instrutor DECIMAL) RETURNS VOID AS $$
+DECLARE 
+    id_instrutor_inserido INTEGER;
+    media_salarial DECIMAL;
+    instrutores_recebem_menos INTEGER DEFAULT 0;
+    total_instrutores INTEGER DEFAULT 0;
+    salario DECIMAL;
+    percentual DECIMAL;
+BEGIN
+    -- Busca a média de salário dos instrutores
+    SELECT AVG(instrutor.salario) INTO media_salarial FROM instrutor;
+    
+    -- Insere novo instrutor e recupera o seu ID
+    INSERT INTO instrutor (nome, salario) 
+        VALUES (nome_instrutor, salario_instrutor) RETURNING id INTO id_instrutor_inserido;
+
+    -- Verifica se o salário do instrutor é maior que a média salarial e, se for, insere o log
+    IF salario_instrutor > media_salarial THEN
+        INSERT INTO log_instrutores (informacao) 
+            VALUES (nome_instrutor || ' recebe acima da média');
+    END IF;
+
+    -- Verifica quantos instrutores recebem menos que o novo instrutor
+    FOR salario IN SELECT instrutor.salario FROM instrutor WHERE id != id_instrutor_inserido LOOP
+        total_instrutores := total_instrutores + 1;
+        IF salario_instrutor > salario THEN
+            instrutores_recebem_menos := instrutores_recebem_menos + 1;
+        END IF;
+    END LOOP;
+
+    -- Calcula o percentual de instrutores que recebem menos que o novo instrutor e insere no log
+    percentual := instrutores_recebem_menos::DECIMAL / total_instrutores::DECIMAL * 100;
+    INSERT INTO log_instrutores (informacao) 
+        VALUES (nome_instrutor || ' recebe mais do que ' || percentual || '% da grade de instrutores'); 
+END;
+$$ LANGUAGE plpgsql;
+
+-- Testando a função cria_instrutor
+SELECT cria_instrutor('Josiel Alemão', 1000);
+
+-- Verificar as tabelas após a execução da função
+SELECT * FROM instrutor;
+SELECT * FROM log_instrutores;
