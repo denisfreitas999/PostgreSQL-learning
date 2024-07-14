9ffc7963e25fa3b194299c72eb512efccd85d9b0
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-13 22:25:22 -0300

    feat: :sparkles: "Usando blocos anônimos com DO, rodando scripts pontuais em PLpgSQL, preparando funções, entendendo boas práticas, conhecendo ferramentas como DataGrip e EMS.

--- course-05/sql/comandos.sql
+++ course-05/sql/comandos.sql
@@ -374,3 +374,60 @@ INSERT INTO instrutor (nome, salario) VALUES ('João', 6000);
 --##########################################################
 --######################## Etapa 05 ########################
 --##########################################################
+
+-- Lidando com blocos anônimos
+
+SELECT * FROM instrutor;
+
+DO $$
+    DECLARE
+        cursor_salarios refcursor;
+        salario DECIMAL;
+        total_instrutores INTEGER DEFAULT 0;
+        instrutor_recebe_menos INTEGER DEFAULT 0;
+        percentual DECIMAL;
+    BEGIN
+        SELECT instrutores_internos(12) INTO cursor_salarios; 
+        LOOP
+            FETCH cursor_salarios INTO salario;
+            EXIT WHEN NOT FOUND;
+            total_instrutores := total_instrutores + 1;
+
+            IF 600::DECIMAL > salario THEN
+                instrutor_recebe_menos := instrutor_recebe_menos + 1;
+            END IF;    
+        END LOOP;
+        percentual = instrutor_recebe_menos::DECIMAL / total_instrutores::DECIMAL * 100; 
+
+        RAISE NOTICE 'Percentual: %', percentual; 
+     END;   
+$$;
+
+
+DO $$
+    DECLARE
+        cursor_salarios refcursor;
+        salario DECIMAL;
+        total_instrutores INTEGER DEFAULT 0;
+        instrutor_recebe_menos INTEGER DEFAULT 0;
+        percentual DECIMAL(5,2);
+    BEGIN
+        SELECT instrutores_internos(12) INTO cursor_salarios; 
+        LOOP
+            FETCH cursor_salarios INTO salario;
+            EXIT WHEN NOT FOUND;
+            total_instrutores := total_instrutores + 1;
+
+            IF 600::DECIMAL > salario THEN
+                instrutor_recebe_menos := instrutor_recebe_menos + 1;
+            END IF;    
+        END LOOP;
+        percentual = instrutor_recebe_menos::DECIMAL / total_instrutores::DECIMAL * 100; 
+
+        RAISE NOTICE 'Percentual: % %%', percentual; 
+     END;   
+$$;
+
+
+
+
